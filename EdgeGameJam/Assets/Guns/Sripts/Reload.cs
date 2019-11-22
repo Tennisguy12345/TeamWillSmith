515efdf0aa5d37fdc09bdcedfd5df5256104e118
hcschwieber23 <56989194+hcschwieber23@users.noreply.github.com>
2019-11-21 18:59:02 -0600

    Harrison2

---
+++ EdgeGameJam/Assets/Guns/Sripts/Reload.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reload : MonoBehaviour
+{
+    public GameObject Magazine;
+    public Vector3 Loaded;
+    public GameObject unloaded;
+    public GameObject loaded;
+    public float ReloadControll;
+    public float Release;
+    public float Start1;
+    public float movement = -0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Start1 = 1;
+        if (Start1 == 1)
+        {
+            Instantiate(Magazine, Loaded, Quaternion.identity);
+            Debug.Log("Test1");
+            Start1 = 0;
+            Debug.Log("Test2");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Loaded = loaded.transform.position;
+        
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ReloadControll = 1;
+        }
+        if (ReloadControll == 1)
+        {
+            DoMath();
+        }
+        if (Release == 2)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+        }
+        if (Release == 3)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+        }
+        if (Release == 4)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+        }
+        if (Release == 5)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+        }
+        if (Release == 6)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+        }
+        if (Release == 7)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+        }
+        if (Release == 8)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+        }
+        if (Release == 9)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+        }
+        if (Release == 10)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+        }
+        if (Release == 11)
+        {
+            Magazine.transform.Translate(0, movement, 0);
+            Destroy(Magazine);
+        }
+        if (Release == 12)
+        {
+            ReloadControll = 0;
+            Release = 0;
+        }
+
+    }
+    void DoMath()
+    {
+        Release += 1;
+    }
+}
---
+++ EdgeGameJam/Assets/Guns/Sripts/TriggerRotate.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Time;
+    public float TimeControl;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            TimeControl = 1;
+        }
+        if (TimeControl == 1)
+        {
+            DoMath();
+        }
+        if (Time==1f)
+        {
+            transform.Rotate(5f, 0f, 0f);
+        }
+        if (Time == 2f)
+        {
+            transform.Rotate(5f, 0f, 0f);
+        }
+        if (Time == 3f)
+        {
+            transform.Rotate(5f, 0f, 0f);
+        }
+        if (Time == 4f)
+        {
+            transform.Rotate(5f, 0f, 0f);
+        }
+        if (Time == 5f)
+        {
+            transform.Rotate(5f, 0f, 0f);
+        }
+        if (Time == 6f)
+        {
+            transform.Rotate(5f, 0f, 0f);
+        }
+        if (Time == 7f)
+        {
+            transform.Rotate(5f, 0f, 0f);
+        }
+        if (Time == 8f)
+        {
+            transform.Rotate(-5f, 0f, 0f);
+        }
+        if (Time == 9f)
+        {
+            transform.Rotate(-5f, 0f, 0f);
+        }
+        if (Time == 10f)
+        {
+            transform.Rotate(-5f, 0f, 0f);
+        }
+        if (Time == 11f)
+        {
+            transform.Rotate(-5f, 0f, 0f);
+        }
+        if (Time == 12f)
+        {
+            transform.Rotate(-5f, 0f, 0f);
+        }
+        if (Time == 13f)
+        {
+            transform.Rotate(-5f, 0f, 0f);
+        }
+        if (Time == 14f)
+        {
+            transform.Rotate(-5f, 0f, 0f);
+        }
+        if (Time == 15f)
+        {
+            TimeControl=0;
+            Time = 0;
+        }
+
+    }
+    void DoMath()
+    {
+        Time += 1;
+        //Debug.Log("Go");
+    }
+}
