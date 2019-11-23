40dc5f6fc6f450ea9f1f53f13d531d008d07efc0
Tennisguy12345 <57269502+Tennisguy12345@users.noreply.github.com>
2019-11-23 17:17:28 -0600

    Update playermovement.cs

--- EdgeGameJam/Assets/playermovement.cs
+++ EdgeGameJam/Assets/playermovement.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class playermovement : MonoBehaviour
 {
     public Rigidbody Player;
+    public GameObject nodoublejump;
+    private bool isgrounded;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isgrounded == true)
+        {
+            
+            if (Input.GetButtonDown("Jump"))
+            {
+                Player.AddRelativeForce(new Vector3(0, 100000, 0));
+
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         if (Input.GetKey("d"))
-            Player.AddRelativeForce(new Vector3(10, 0, 0));
+            Player.transform.Translate(new Vector3(10, 0, 0));
         if (Input.GetKey("a"))
-            Player.AddRelativeForce(new Vector3(-10, 0, 0));
+            Player.transform.Translate(new Vector3(-10, 0, 0));
 
 
+    }
+    void OnCollisionEnter(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Grass Level")
+        {
+            isgrounded = true;
+            
+        }
+    }
+    void OnCollisionExit(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Grass Level")
+        {
+            isgrounded = false;
+        }
     }
 }
