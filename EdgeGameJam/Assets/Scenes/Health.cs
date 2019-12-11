28e4991352fcee3844bc6a59868f6d25ef6a9d02
hcschwieber23 <56989194+hcschwieber23@users.noreply.github.com>
2019-12-10 19:18:42 -0600

    k

---
+++ EdgeGameJam/Assets/Scenes/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health = 20;
+    public float Speed = 1;
+    
+    void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+
+        if (health>0)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                health -= 1;
+            }
+        }
+
+        
+
+
+    }
+    void Update()
+    {
+
+    }
+}
